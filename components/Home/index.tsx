dfeb72b9dfbb71d6806b82b89f510d742ff3e899
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2025-05-02 13:45:57 +0000

    Title changing

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4 space-y-8">
       <h1 className="text-3xl font-bold text-center">
-        Monad Farcaster MiniApp Template
+      PDFReader
       </h1>
       <div className="w-full max-w-4xl space-y-6">
         <User />
